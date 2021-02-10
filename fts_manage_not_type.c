7d33a305cd5ef00f6063d92640baca6f7fbf3aeb
Arrigo Violini <aviolini@e3r4p8.42roma.it>
2021-02-10 10:46:16 +0100

    ok

--- fts_manage_not_type.c
+++ fts_manage_not_type.c
@@ -6,9 +6,10 @@
 /*   By: aviolini <aviolini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 23:15:14 by aviolini          #+#    #+#             */
-/*   Updated: 2021/02/09 17:28:26 by aviolini         ###   ########.fr       */
+/*   Updated: 2021/02/10 10:45:42 by aviolini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "libftprintf.h"
 
@@ -16,7 +17,26 @@ void	ft_perc(t_strutt *strutt, va_list ap)
 {
 	(void)strutt;
 	(void)ap;
-	ft_putchar("%", 1, strutt);
+
+	if(strutt->flag_minus == 0)
+	{
+		if(strutt->flag_zero)
+			{
+				ft_zero_nbr(strutt->width - 1, strutt);
+				ft_putchar("%", 1, strutt);
+				return ;
+
+			}
+		ft_space_nbr(strutt->width - 1, strutt);
+		ft_putchar("%", 1, strutt);
+	}
+	if(strutt->flag_minus == 1)
+	{
+		ft_putchar("%", 1, strutt);
+		ft_space_nbr(strutt->width - 1, strutt);
+	}
+
+
 	//strutt->prev_was_perc = 1;
 }
 
